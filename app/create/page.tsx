b3bdae9729cdf9f3dfc2b9b9d2adaf28a55f10f8
qreedence <eden.yusof.ioannidis@gmail.com>
2026-03-14 12:33:55 +0100

    Implement draft autosave for create/edit recipe

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,5 +1,19 @@
-import { RecipeForm } from "@/components/create/recipe-form"
+'use client'
+
+import { RecipeForm } from '@/components/create/recipe-form'
+import { useDraftAutosave } from '@/hooks/use-draft-autosave'
 
 export default function CreatePage() {
-  return <RecipeForm mode="create" />
+  const { loadedDraft, isLoaded, persistDraft, clearDraft } = useDraftAutosave('create')
+
+  if (!isLoaded) return null
+
+  return (
+    <RecipeForm
+      mode="create"
+      initialDraft={loadedDraft}
+      onPersistDraft={persistDraft}
+      onClearDraft={clearDraft}
+    />
+  )
 }
